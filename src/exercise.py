45db020ef63f083a9d865689d1bf618cbe896fdc
MrsVicki <vjeffery@bridgend.ac.uk>
2020-09-07 11:43:25 +0000

    I am convinced I am right

--- src/exercise.py
+++ src/exercise.py
@@ -1,6 +1,7 @@
-def main():
-    #write your code below this line
-    message = input("Write a message...")
 
+def main():
+  message = input("Write a message:")
+  
+  print (message) 
 if __name__ == '__main__':
     main()
